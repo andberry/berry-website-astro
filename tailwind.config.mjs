9eed63d17ee546287af30054490ed942a5bc5c59
Andrea Berardi <andrea.berardi@verbinteractive.com>
2023-10-21 02:42:38 +0200

    Tailwind: adding colors and screens

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,22 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    colors: {
+      white: "#FFFFFF",
+      black0: "#000",
+      lightgray: "#f2f3f6",
+      purple: "#5b21b6",
+      pink: "#FF10F0",
+      lime: "#90FF5D",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1440px",
+      "3xl": "1680px",
+    },
     fontFamily: {
       exo2: ['"Exo 2"', "sans-serif"],
       merryweather: ["Merriweather", "serif"],
