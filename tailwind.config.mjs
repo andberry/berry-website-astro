e45636c5b44658128eae04d69121748909a0e01f
Andrea Berardi <andrea.berardi@verbinteractive.com>
2023-10-21 02:13:55 +0200

    Playing with fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      exo2: ['"Exo 2"', "sans-serif"],
+      domine: ["Domine", "serif"],
+      playfairDisplay: ['"Playfair Display"', "serif"],
+      titillium: ["Titillium Web", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
