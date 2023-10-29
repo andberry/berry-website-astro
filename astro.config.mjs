5e7521de1f19c5f9df9cb3b23355fb3133d11d95
Andrea Berardi <andrea.berardi@verbinteractive.com>
2023-10-29 13:01:56 +0100

    Adding langs to astro/shiki config

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,19 @@ export default defineConfig({
       // Add custom languages
       // Note: Shiki has countless langs built-in, including .astro!
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: [],
+      langs: [
+        "astro",
+        "css",
+        "sass",
+        "scss",
+        "ts",
+        "js",
+        "json",
+        "html",
+        "php",
+        "python",
+        "twig",
+      ],
       // Enable word wrap to prevent horizontal scrolling
       wrap: true,
     },
