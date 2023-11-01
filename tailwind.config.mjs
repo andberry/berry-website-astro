d34f0f8fdbb6cb9c3e1e9186b92d840fcf74c65a
Andrea Berardi <andrea.berardi@verbinteractive.com>
2023-11-01 09:39:36 +0100

    Style for Dark mode as default

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     colors: {
       white: "#FFFFFF",
       black0: "#000",
       lightgray: "#f2f3f6",
       mediumgray: "#d9dadd",
+      darkgray: "#aaa",
       purple: "#5b21b6",
       pink: "#FF10F0",
       lime: "#90FF5D",
+      black: "#1f1f1f",
 
       //
       ultraviolet: "#52489C",
@@ -41,13 +44,18 @@ export default {
       exo2: ['"Exo 2"', "sans-serif"],
       merryweather: ["Merriweather", "serif"],
       mono: ['"JetBrains Mono"', "monospace"],
+      literata: ["Literata", "serif"],
+      montserrat: ["Montserrat", "sans-serif"],
     },
     extend: {
       fontSize: {
         clamphero: "clamp(4rem, -1.75rem + 10vw, 7.25rem)",
-        clamph1: "clamp(3.2rem, -1.786rem + 9.762vw, 7rem)",
-        clamph2: "clamp(2.6rem, -1.714rem + 8.571vw, 6rem)",
-        clamph3: "clamp(2.2rem, -1.036rem + 6.429vw, 4.75rem)",
+        // clamph1: "clamp(3.2rem, -1.786rem + 9.762vw, 7rem)",
+        clamph1: "clamp(2.625rem, 1.225rem + 3.5vw, 4.375rem)",
+        // clamph2: "clamp(2.6rem, -1.714rem + 8.571vw, 6rem)",
+        clamph2: "clamp(2.5rem, 1.1rem + 3.5vw, 4.25rem)",
+        //clamph3: "clamp(2.2rem, -1.036rem + 6.429vw, 4.75rem)",
+        clamph3: "clamp(1.5rem, 0.9rem + 1.5vw, 2.25rem)",
       },
     },
   },
