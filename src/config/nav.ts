ed926085ec31c95a44936837696cd51ea3d284bd
Andrea Berardi <andrea.berardi@verbinteractive.com>
2023-11-02 01:13:47 +0100

    nav from js to ts

--- src/config/nav.ts
+++ src/config/nav.ts
@@ -1,4 +1,9 @@
-export const nav = [
+interface INav {
+  url: string;
+  title: string;
+}
+
+export const nav: INav[] = [
   { url: "/", title: "Home" },
   // { url: "/skills", title: "Skill" },
   { url: "/blog", title: "Blog" },
