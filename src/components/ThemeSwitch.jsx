43651b537eeac3ed174f196ebb1527b6d3ae3fc0
Andrea Berardi <andrea.berardi@verbinteractive.com>
2023-11-12 23:22:22 +0100

    Implementing ThemeSwitcher: BaseLayout, body class, localStorage and react component

--- src/components/ThemeSwitch.jsx
+++ src/components/ThemeSwitch.jsx
@@ -2,35 +2,34 @@ import { useState } from "react";
 import { Switch } from "@headlessui/react";
 
 export const ThemeSwitch = () => {
-  const [enabled, setEnabled] = useState(true);
-  const switchCB = (val) => {
-    console.log("switchCB: ", val);
+  const [themeIsDark, setThemeIsDark] = useState(
+    document.documentElement.classList.contains("dark")
+  );
+
+  const themeSwitchCallBack = (val) => {
     if (val === true) {
       document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
     } else {
       document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
     }
-    setEnabled(val);
+    setThemeIsDark(val);
   };
 
   return (
-    <div className="flex gap-2 items-center">
-      <span className="text-xs">Light</span>
-
-      <Switch
-        checked={enabled}
-        onChange={switchCB}
+    <Switch
+      checked={themeIsDark}
+      onChange={themeSwitchCallBack}
+      className={`${
+        themeIsDark ? "bg-raisingBlack" : "bg-white border-2 border-[black0]"
+      } relative inline-flex h-6 w-11 items-center rounded-full`}
+    >
+      <span
         className={`${
-          enabled ? "bg-raisingBlack" : "bg-black0"
-        } relative inline-flex h-6 w-11 items-center rounded-full`}
-      >
-        <span
-          className={`${
-            enabled ? "translate-x-6" : "translate-x-1"
-          } inline-block h-4 w-4 transform rounded-full bg-greenyellow transition`}
-        />
-      </Switch>
-      <span className="text-xs">Dark</span>
-    </div>
+          themeIsDark ? "translate-x-6 bg-pink2" : "translate-x-1 bg-purple"
+        } inline-block h-4 w-4 transform rounded-full transition duration-1000`}
+      />
+    </Switch>
   );
 };
