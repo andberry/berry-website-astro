d2bab52b700464a6e776fddf62587f253cc20c07
Andrea Berardi <andrea.berardi@verbinteractive.com>
2025-08-23 01:50:33 +0200

    Refactor activePageLinksUpdate and theme as separate scripts loaded in BaseLayout

---
+++ src/scripts/activePageLinksUpdate.ts
@@ -0,0 +1,18 @@
+/**
+ * Active Page link
+ * Since we're using View transition,
+ * we need custom js code to run after every page swap
+ * to correctly update active link
+ */
+document.addEventListener("astro:after-swap", () => {
+  const currentUrl = window.location.href;
+  const mainNavLinks = document.querySelectorAll(
+    "#desktop-main-nav .link, #mobile-main-nav .link",
+  );
+  for (const link of Array.from(mainNavLinks) as HTMLAnchorElement[]) {
+    link.classList.remove("dark:text-greenyellow", "text-purple");
+    if (link.href === currentUrl) {
+      link.classList.add("dark:text-greenyellow", "text-purple");
+    }
+  }
+});
