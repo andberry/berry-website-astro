d3977dbf64c6b324189c19bc70826e2c00360bbc
Andrea Berardi <andrea.berardi@verbinteractive.com>
2025-09-13 00:47:44 +0200

    menuAnimation: adding animationsDefaults

--- src/scripts/menuAnimation.ts
+++ src/scripts/menuAnimation.ts
@@ -1,10 +1,15 @@
 import { gsap } from "gsap"
 
+
+const animationsDefaults = {
+  duration: 0.3
+}
+
 export const desktopMenuAnimation = () => {
   const desktopNavLinks = document.querySelectorAll('#desktop-main-nav .link');
 
   const mainTl = gsap.timeline({delay: 0.5})
-  mainTl.fromTo(desktopNavLinks, {autoAlpha: 0, y: 3}, {autoAlpha: 1, y: 0, ease: 'power3.out', duration: 0.25, stagger: 0.12})
+  mainTl.fromTo(desktopNavLinks, {autoAlpha: 0, y: 3}, {autoAlpha: 1, y: 0, ease: 'power3.out', duration: 0.3, stagger: 0.06})
 }
 
 export const setupMobileMenuAnimation = () => {
@@ -16,7 +21,7 @@ export const setupMobileMenuAnimation = () => {
   const mobileSocials = document.querySelector('#mobile-socials')
   const mobileThemeSwitch = document.querySelector('#mobile-theme-switch')
 
-  const tl = gsap.timeline({paused: true, delay: 1, defaults: {duration: 0.3}})
+  const tl = gsap.timeline({paused: true, delay: 1, defaults: animationsDefaults})
   tl.to(mobileMenuOpenTrigger, {y: -10, opacity: 0, ease: 'power3.inOut'})
   tl.to(mobileMenuBgOverlay, {autoAlpha: 1}, '>-0.2')
   tl.to(mobileMenuPanel, {x: 0}, '>-0.2')
@@ -43,9 +48,9 @@ export const setupMobileMenuAnimation = () => {
 export const setupLogoAnimation = () => {
   const logo = document.querySelector('#page-header .js-logoLink')
 
-  const logoTl = gsap.timeline({repeat: -1, delay: 2});
-  logoTl.fromTo(logo,{y: '100%', opacity: 0}, {opacity: 1, y: 0, duration: 0.4, ease: 'expo.out'});
-  logoTl.to(logo,{opacity: 0, y: '100%', duration: 0.4, ease: 'expo.out'}, '>+12')
+  const logoTl = gsap.timeline({repeat: -1, delay: 0.5, defaults: animationsDefaults});
+  logoTl.fromTo(logo,{y: '100%', opacity: 0}, {opacity: 1, y: 0, ease: 'expo.out'});
+  logoTl.to(logo,{opacity: 0, y: '100%', duration: 0.3, ease: 'expo.inOut'}, '>+9')
 
   logoTl.play()
 }
